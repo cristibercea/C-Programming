20662d4cdcda7207fd7aec3341a3cb8076003258
Bercea Cristian <101464511+cristibercea@users.noreply.github.com>
2024-01-05 15:20:19 +0200

    Delete FinancialApp_v2 directory

--- FinancialApp_v2/tests.h
+++
@@ -1,9 +0,0 @@
-#ifndef TESTS_H_INCLUDED
-#define TESTS_H_INCLUDED
-
-    #include "interface.h"
-    #include "services.h"
-    #include "domain.h"
-
-    void testbench();
-#endif // TESTS_H_INCLUDED
